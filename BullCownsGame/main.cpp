722394adb9043a94b269e7a29df5d26556ced858
Paulo <paulotarsobranco@hotmail.com>
2017-11-21 14:05:14 -0300

    continuação do jogo

--- BullCownsGame/main.cpp
+++ BullCownsGame/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
-	cout << "Welcome to Bulls and Cows\n";
-	return 0;
+	// Introduce the Game
+	constexpr int WORD_LENGTH = 5; 
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking off?\n";
+	cout << endl;
+
+	// get a guess from the player 
+	string Guess = "";
+	cout << "Enter your Guess: ";
+	getline(cin, Guess);
+	// return the guess to the player
+	cout << "Your guess was: " << Guess << endl;
+	
+	// get a guess from the player 
+	cout << "Enter your Guess: ";
+	getline(cin, Guess);
+	// return the guess to the player
+	cout << "Your guess was: " << Guess << endl;
+
+
+	
+	return 0; 
 }	
